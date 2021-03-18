c7e8b7598e8163285e7530c668196a37f77ebb64
Sachin Sachdeva <sachdevasachin434@gmail.com>
2021-03-18 08:43:11 +0530

    PubSub Implementation

--- app/extract/service/service.py
+++ app/extract/service/service.py
@@ -2,6 +2,9 @@
     Service Layer for book detail extraction.
 """
 import requests
+import os
+import pickle
+from google.cloud import pubsub_v1
 
 class GetBookData:
     """
@@ -30,4 +33,29 @@ class GetBookData:
             if "description" in i["volumeInfo"]:
                 book_info["description"] = i["volumeInfo"]["description"]
             output_dict["output"].append(book_info)
+
+        os.system('python gsc_sub.py')
+        os.system('python datastore_sub.py')
+        os.system('python bigquery_sub.py')
+        PublishMessages.publish_messages(os.getenv('PROJECT_ID'), os.getenv('PUBSUB_TOPIC_BigQuery'), output_dict)
+        PublishMessages.publish_messages(os.getenv('PROJECT_ID'), os.getenv('PUBSUB_TOPIC_Datastore'), output_dict)
+        PublishMessages.publish_messages(os.getenv('PROJECT_ID'), os.getenv('PUBSUB_TOPIC_GCS'), output_dict)
         return output_dict
+
+
+class PublishMessages:
+
+    @staticmethod
+    def publish_messages(project_id, topic_id, data):
+        publisher = pubsub_v1.PublisherClient()
+        topic_path = publisher.topic_path(project_id, topic_id)
+
+        # Data must be a bytestring
+        data = pickle.dumps(data).encode('base64', 'strict')
+        # color = pickle.loads(b64_color.decode('base64', 'strict'))
+        data = data.encode("utf-8")
+        # When you publish a message, the client returns a future.
+        future = publisher.publish(topic_path, data)
+        print(future.result())
+
+        print(f"Published messages to {topic_path}.")
---
+++ bigquery_sub.py
@@ -0,0 +1,28 @@
+from google.cloud import pubsub_v1
+
+def receive_messages(project_id, subscription_id, timeout=None):
+    """Receives messages from a pull subscription."""
+
+    subscriber = pubsub_v1.SubscriberClient()
+    # The `subscription_path` method creates a fully qualified identifier
+    # in the form `projects/{project_id}/subscriptions/{subscription_id}`
+    subscription_path = subscriber.subscription_path(project_id, subscription_id)
+
+    def callback(message):
+        print(f"Received {message}.")
+        message.ack()
+
+    streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+    print(f"Listening for messages on {subscription_path}..\n")
+
+    # Wrap subscriber in a 'with' block to automatically call close() when done.
+    with subscriber:
+        try:
+            # When `timeout` is not set, result() will block indefinitely,
+            # unless an exception is encountered first.
+            streaming_pull_future.result(timeout=timeout)
+        except TimeoutError:
+            streaming_pull_future.cancel()
+
+if __name__=="__main__":
+    receive_messages(os.getenv('PROJECT_ID'), os.getenv('SUBSCRIPTION_ID_Bigquery'), timeout=100)
